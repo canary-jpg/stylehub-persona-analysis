8b92ee2170f575ec93219cd736c99f75337965fb
Hazel Donaldson <hazel90.hd@gmail.com>
2026-03-06 13:44:31 -0500

    updates to dbt model

---
+++ stylehub_dbt/target/run/stylehub_dbt/models/staging/stg_products.sql
@@ -0,0 +1,13 @@
+
+  
+  create view "stylehub"."main"."stg_products__dbt_tmp" as (
+    select 
+    product_id,
+    product_name,
+    lower(trim(category)) as category,
+    lower(trim(brand)) as brand,
+    base_price,
+    avg_rating,
+    num_reviews
+from products
+  );
